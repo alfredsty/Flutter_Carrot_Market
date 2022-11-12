f3f5cc699f563c3986174e9eed11e227d8d1a69f
KuroS2 <102028778+alfredsty@users.noreply.github.com>
2022-11-12 23:04:40 +0900

    2022/11/12

---
+++ carrot_market/lib/home_page.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'feed.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // 생성자
+  @override
+  Widget build(BuildContext context) {
+    // 좋아요 여부
+    bool isFavorite = false;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.5,
+        leading: Row(
+          children: [
+            SizedBox(width: 16),
+            Text(
+              '중앙동',
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+            Icon(
+              Icons.keyboard_arrow_down_rounded,
+              color: Colors.black,
+            ),
+          ],
+        ),
+        leadingWidth: 100,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(CupertinoIcons.search, color: Colors.black),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.menu_rounded, color: Colors.black),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(CupertinoIcons.bell, color: Colors.black),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: ListView.separated(
+          itemCount: 10,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 12),
+              child: Feed(),
+            );
+          },
+          separatorBuilder: (context, index) {
+            return Divider();
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Color(0xFFFF7E36),
+        elevation: 1,
+        child: Icon(
+          Icons.add_rounded,
+          size: 36,
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        fixedColor: Colors.black,
+        unselectedItemColor: Colors.black,
+        showUnselectedLabels: true,
+        selectedFontSize: 12,
+        unselectedFontSize: 12,
+        iconSize: 28,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_filled),
+            label: '홈',
+            backgroundColor: Colors.white,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.my_library_books_outlined),
+            label: '동네생활',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.fmd_good_outlined),
+            label: '내 근처',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.chat_bubble_2),
+            label: '채팅',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person_outline,
+            ),
+            label: '나의 당근',
+          ),
+        ],
+        currentIndex: 0,
+      ),
+    );
+  }
+}
